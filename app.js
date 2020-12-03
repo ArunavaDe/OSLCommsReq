a997cee3379cbdcfca302c2f6042fe91d05237fb
arunavaDe <arunava.de@gmail.com>
2020-12-03 16:24:44 -0500

    Heroku Fix 3

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT||3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
